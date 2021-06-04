1462c29a44ce43aa9420db6340560db391a21f21
Jaime McCutcheon <mccutcheon.james@gmail.com>
2021-06-04 09:19:56 +0200

    Added new test function

--- example.py
+++ example.py
@@ -10,5 +10,5 @@ def subtract(a, b):
     return a + b
     
 # uncomment the following test in step 5
-#def test_subtract():
-#    assert subtract(2, 3) == -1
+def test_subtract():
+    assert subtract(2, 3) == -1
